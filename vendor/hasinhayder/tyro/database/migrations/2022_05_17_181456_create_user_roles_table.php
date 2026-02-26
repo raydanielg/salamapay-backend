d9659010e2872108210dff1a4f01c7e1782a59c0
raydanielg <airezra2+1@gmail.com>
2026-02-26 08:28:38 -0800

    commit

--- vendor/hasinhayder/tyro/database/migrations/2022_05_17_181456_create_user_roles_table.php
+++ vendor/hasinhayder/tyro/database/migrations/2022_05_17_181456_create_user_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         $pivot = config('tyro.tables.pivot', 'user_roles');
         Schema::create($pivot, function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('role_id')->constrained(config('tyro.tables.roles', 'roles'))->cascadeOnDelete();
             $table->unique(['user_id', 'role_id']);
             $table->timestamps();
