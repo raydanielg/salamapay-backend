f61436396612b5c4dd46a690a65ee34de72113c8
raydanielg <airezra2+1@gmail.com>
2026-02-26 09:09:19 -0800

    commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'auth.page.key' => \App\Http\Middleware\SecureAuthPageMiddleware::class,
         ]);
 
         //
